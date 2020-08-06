23169fc1e0390bf7214762637aae0927391aa5bb
DamMordekai <46605081+DamMordekai@users.noreply.github.com>
2020-08-06 02:31:52 -0300

    Done
    minor changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 import './App.css';
 import MainContent from './Components/MainContent/MainContent'
 import './App.css'
-require('dotenv').config()
 
-
-console.log(process.env)
 
 function App() {
   return (
